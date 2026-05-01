440e097e21ed6a9d19664734a413c205fa3ed503
glpskumar <151714675+GLPSravanaKumar@users.noreply.github.com>
2026-05-01 09:55:02 +0530

    Fix output redirection for index.html

--- userdata.sh
+++ userdata.sh
@@ -3,4 +3,4 @@ apt update -y
 apt install -y nginx
 systemctl start nginx
 systemctl enable nginx
-echo "Hello World from $(hostname)" > /var/www/html/index.html
+echo "Hello World from $(hostname)" >/var/www/html/index.html
